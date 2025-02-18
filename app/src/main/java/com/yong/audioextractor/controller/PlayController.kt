9888e10693967420644df861fa021123ff9dbc07
Useful <useful@vimosoft.com>
2025-02-18 17:10:05 +0900

    Update PlayController : Added Comments

--- app/src/main/java/com/yong/audioextractor/controller/PlayController.kt
+++ app/src/main/java/com/yong/audioextractor/controller/PlayController.kt
@@ -10,12 +10,22 @@ import com.bluelinelabs.conductor.Controller
 import com.google.android.material.button.MaterialButton
 import com.yong.audioextractor.R
 
+/**
+ * PlayController
+ * - Video 재생 기능을 구현한 Controller
+ * - 각 Button을 통해 Video Play Pause Stop 상태 제어
+ * - ProgressBar를 통해 Video Play 진행도 표시
+ */
 class PlayController: Controller() {
+    // UI Elements
+    // Buttons
     private lateinit var btnExtract: MaterialButton
     private lateinit var btnPause: MaterialButton
     private lateinit var btnPlay: MaterialButton
     private lateinit var btnStop: MaterialButton
+    // Video 재생 진행도 ProgressBar
     private lateinit var progressPlay: ProgressBar
+    // Video가 재생되는 TextureView
     private lateinit var textureView: TextureView
 
     override fun onCreateView(
@@ -23,6 +33,7 @@ class PlayController: Controller() {
         container: ViewGroup,
         savedViewState: Bundle?
     ): View {
+        // View 초기화
         val view = inflater.inflate(R.layout.controller_play, container, false)
         btnExtract = view.findViewById(R.id.play_btn_extract_audio)
         btnPause = view.findViewById(R.id.play_btn_pause)
@@ -31,6 +42,7 @@ class PlayController: Controller() {
         progressPlay = view.findViewById(R.id.play_progress)
         textureView = view.findViewById(R.id.play_texture_view)
 
+        // Button OnClickListener 지정
         btnExtract.setOnClickListener(btnListener)
         btnPause.setOnClickListener(btnListener)
         btnPlay.setOnClickListener(btnListener)
@@ -39,20 +51,25 @@ class PlayController: Controller() {
         return view
     }
 
+    // Button OnClickListener
     private val btnListener = View.OnClickListener { view ->
         when(view) {
+            // Extract Audio
             btnExtract -> {
                 // TODO: Go to ExtractController
             }
 
+            // Video Play Pause
             btnPause -> {
                 // TODO: Pause Video Play
             }
 
+            // Video Play Start
             btnPlay -> {
                 // TODO: Start Video Play
             }
 
+            // Video Play Stop
             btnStop -> {
                 // TODO: Stop Video Play
             }
