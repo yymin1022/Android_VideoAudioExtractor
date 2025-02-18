91634454688d7094f173cc987a146367b095d09b
Useful <useful@vimosoft.com>
2025-02-18 15:56:47 +0900

    Added ExtractController

---
+++ app/src/main/java/com/yong/audioextractor/controller/ExtractController.kt
@@ -0,0 +1,17 @@
+package com.yong.audioextractor.controller
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bluelinelabs.conductor.Controller
+
+class ExtractController: Controller() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup,
+        savedViewState: Bundle?
+    ): View {
+        return container
+    }
+}
