13e6196aade7f9627c6d20f7238c207059d20b56
Useful <useful@vimosoft.com>
2025-02-18 15:39:33 +0900

    Update MainActivity : Defined default routeR

--- app/src/main/java/com/yong/audioextractor/activity/MainActivity.kt
+++ app/src/main/java/com/yong/audioextractor/activity/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.yong.audioextractor
+package com.yong.audioextractor.activity
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.yong.audioextractor.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+    private lateinit var router: Router
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val controllerContainer = findViewById<ViewGroup>(R.id.main_controller_container)
+        router = Conductor.attachRouter(this, controllerContainer, savedInstanceState)
+        
+        if(!router.hasRootController()) {
+
         }
     }
 }
