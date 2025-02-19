b250d07478de4e8ff843f6558f03a4e02813dd92
Useful <useful@vimosoft.com>
2025-02-19 10:51:52 +0900

    Update VideoDecoder Model : Defined basic functions

--- app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
+++ app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
@@ -1,4 +1,17 @@
 package com.yong.audioextractor.model
 
+import android.view.Surface
+
 class VideoDecoder {
+    fun pauseVideo() {
+
+    }
+
+    fun playVideo(surface: Surface) {
+
+    }
+
+    fun stopVideo() {
+
+    }
 }
