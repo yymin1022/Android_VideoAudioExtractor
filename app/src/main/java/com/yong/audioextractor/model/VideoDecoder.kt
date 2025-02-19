9423bbe62974f44947a7514b3f53f86663f26739
Useful <useful@vimosoft.com>
2025-02-19 15:22:57 +0900

    Update VideoDecoder Model : Added Comments

--- app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
+++ app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
@@ -25,52 +25,73 @@ class VideoDecoder(
     var isPaused = false
     var isPlaying = false
 
+    // Decoding 시작
     fun startDecoding(surface: Surface) {
+        // MediaExtractor 초기화
+        // Video FD에서 파일을 읽어 Source로 지정
         initExtractor()
 
+        // MediaExtractor를 통해 Source 내에서 Video Track 탐색
+        // Track이 Null인 경우 없는 것이므로 Exception 발생
         val videoTrack = getVideoTrack() ?: throw Exception("No Video Track")
+        // 탐색한 Track으로 MediaCodec Decoder 초기화
         initDecoder(videoTrack, surface)
 
+        // Decoder 시작
         mediaCodec.start()
         isPlaying = true
     }
 
+    // 일시 정지된 Decoding을 계속 진행
     fun resumeDecoding() {
         isPaused = false
     }
 
+    // Decoding 일시 정지
     fun pauseDecoding() {
         isPaused = true
     }
 
+    // Decoding 종료
     fun stopDecoding() {
+        // Decoder 종료 및 해제
         isPlaying = false
         mediaCodec.stop()
         mediaCodec.release()
         mediaExtractor.release()
     }
 
+    // MediaExtractor 초기화
     private fun initExtractor() {
         mediaExtractor = MediaExtractor()
+        // Video FD에서 파일을 읽어 Source로 지정
         mediaExtractor.setDataSource(videoFd, fileOffset, fileLength)
     }
 
+    // MediaExtractor를 통해 Source 내에서 Video Track 탐색
     private fun getVideoTrack(): Int? {
+        // MediaExtractor 내 모든 Track을 탐색
         for(i in 0 until mediaExtractor.trackCount) {
             val trackFormat = mediaExtractor.getTrackFormat(i)
             val trackType = trackFormat.getString(MediaFormat.KEY_MIME) ?: continue
 
+            // video/avc MIME Type을 갖는 Track 지정
             if(trackType == MediaFormat.MIMETYPE_VIDEO_AVC) {
                 mediaExtractor.selectTrack(i)
                 return i
             }
         }
 
+        // 정해진 Track을 찾지 못한 경우 Null 반환
         return null
     }
 
+    // 탐색한 Track으로 MediaCodec Decoder 초기화
     private fun initDecoder(trackNum: Int, surface: Surface) {
+        // video/avc MIME Type 지정
         mediaCodec = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+        // 탐색한 Track을 지정과 렌더링할 Surface를 지정
+        // Crypto와 Flag는 지정하지 않음
         mediaCodec.configure(mediaExtractor.getTrackFormat(trackNum), surface, null, 0)
     }
 }
