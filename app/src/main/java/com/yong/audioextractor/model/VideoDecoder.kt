8cab649ad344f59c1c347d2d51fc23d9788bd6d9
Useful <useful@vimosoft.com>
2025-02-19 14:17:08 +0900

    Update VideoDecoder Model : Added Comments

--- app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
+++ app/src/main/java/com/yong/audioextractor/model/VideoDecoder.kt
@@ -3,20 +3,29 @@ package com.yong.audioextractor.model
 import android.view.Surface
 import java.io.FileDescriptor
 
+/**
+ * VideoDecoder
+ * - Video를 Decode하고 재생하기 위한 Model
+ */
 class VideoDecoder(
+    // Video File에 접근하기 위한 FD 파라미터
     private val videoFd: FileDescriptor,
     private val fileOffset: Long,
     private val fileLength: Long
 ) {
+    // 재생 중 상태를 표기하기 위한 Field
+    // Pause 상태에서는 true 값음
+    var isPlaying = false
+
+    fun playVideo(surface: Surface) {
+        isPlaying = true
+    }
+
     fun pauseVideo() {
 
     }
 
-    fun playVideo(surface: Surface) {
-
-    }
-
     fun stopVideo() {
-
+        isPlaying = false
     }
 }
