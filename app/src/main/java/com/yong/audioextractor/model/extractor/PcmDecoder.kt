7769e00280018c7c6c574593d93030068c1fe5b2
Useful <useful@vimosoft.com>
2025-02-21 17:59:56 +0900

    Update PcmDecoder Model : Added MediaCodec Initialization

--- app/src/main/java/com/yong/audioextractor/model/extractor/PcmDecoder.kt
+++ app/src/main/java/com/yong/audioextractor/model/extractor/PcmDecoder.kt
@@ -1,12 +1,27 @@
 package com.yong.audioextractor.model.extractor
 
+import android.media.MediaCodec
 import android.media.MediaExtractor
+import android.media.MediaFormat
 import java.nio.ByteBuffer
 
 class PcmDecoder(
-    private val extractor: MediaExtractor
+    private val mediaExtractor: MediaExtractor
 ) {
+    private lateinit var mediaCodec: MediaCodec
+
     fun decodePcm(): List<ByteBuffer> {
+        initDecoder()
         return emptyList()
+    }
+
+    private fun initDecoder() {
+        val audioTrackNum = mediaExtractor.sampleTrackIndex
+        val audioFormat = mediaExtractor.getTrackFormat(audioTrackNum)
+        val audioMimeType = audioFormat.getString(MediaFormat.KEY_MIME) ?: throw Exception("Unknown audio type")
+        mediaCodec = MediaCodec.createDecoderByType(audioMimeType)
+
+        mediaCodec.configure(audioFormat, null, null, 0)
+        mediaCodec.start()
     }
 }
