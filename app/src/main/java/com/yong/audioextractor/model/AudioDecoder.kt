a03091dbcaa070233fe9c23610f599831246e4a0
Useful <useful@vimosoft.com>
2025-02-25 18:04:42 +0900

    Added CommonAudioDecoder Model

---
+++ app/src/main/java/com/yong/audioextractor/model/AudioDecoder.kt
@@ -0,0 +1,10 @@
+package com.yong.audioextractor.model
+
+/**
+ * AudioDecoder
+ * - Decoding이 필요한 Audio Track이 담긴
+ * - MediaExtractor와 Track Number를 전달받고,
+ * - Decoding을 진행한 Output으로 Callback 함수를 호출하는 Model
+ */
+class AudioDecoder {
+}
