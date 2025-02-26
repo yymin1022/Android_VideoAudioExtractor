40f9339b1a34b041c019a1550db92a2f6d7f3a47
Useful <useful@vimosoft.com>
2025-02-26 17:31:14 +0900

    Update VideoDecoder Model : Cleaned UP

--- app/src/main/java/com/yong/audioextractor/model/player/VideoDecoder.kt
+++ app/src/main/java/com/yong/audioextractor/model/player/VideoDecoder.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 /**
